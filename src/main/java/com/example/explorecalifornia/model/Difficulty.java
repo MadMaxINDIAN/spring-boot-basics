34ff7a3e93a40ca89f07006fda02eb4e753fc9cd
Naman Khater <56634706+MadMaxINDIAN@users.noreply.github.com>
2025-09-01 00:32:47 +0530

    Add Difficulty, Region, Tour, and TourPackage models with necessary fields and methods

---
+++ src/main/java/com/example/explorecalifornia/model/Difficulty.java
@@ -0,0 +1,14 @@
+package com.example.explorecalifornia.model;
+
+/**
+ * Created by maryellenbowman on 7/3/16.
+ */
+
+/**
+ * Enumeration of the level of effort.
+ *
+ * Created by Naman Khater
+ */
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
